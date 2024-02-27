9693c1d70e536d0c20fb85e607ca8e5cd67793eb
Vedansh Kandpal <95868825+Vedansh-Kandpal@users.noreply.github.com>
2024-02-27 16:08:21 +0000

    variables an₫ comments

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,7 +2,21 @@ const userID = 123;
 let userName="vedansh";
 var userEmail="vedansh@gmail.com";
 userContact = 8000000140
+let userAddress;
+console.log(userAddress);
+// single line comments 
+// in JavaScript if we declare a variable and not assign any value to it then it will shows undefined 
 
+
+
+// multi line comments 
+
+/*
+prefer not to use var
+because of in issue in block scope and functional scope
+*/
+
+/*_____________________________________________________________________________*/
 /*
 
 userID=321     error: TypeError: Assignment to constant variable.
@@ -16,8 +30,3 @@ userEmail="vedanshkandpal@gmail.com"
 userContact=7000000052
 
 console.table([userID, userName, userEmail ,userContact])
-
-/*
-prefer not to use var
-because of in issue in block scope and functional scope
-*/
