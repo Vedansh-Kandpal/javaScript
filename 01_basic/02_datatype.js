5eb8fced6ecab786314ee948b3492405dbe181db
Vedansh Kandpal <95868825+Vedansh-Kandpal@users.noreply.github.com>
2024-02-27 16:39:28 +0000

    some basics of js, datatype, typeof

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,47 @@
+"use strict";      //treat all js code as newer version
+
+/*
+
+alert(3+3)     if we run using node it shows arror 
+
+*/
+
+console.log(3
+    +3)      // code is correct but not readable so don't use
+
+
+
+
+
+/*
+variables
+
+1. number
+ex: let  num = 10;
+
+2. string
+ex: let str = "vedansh"
+
+3. boolean
+ex: let bool = true/false
+
+4. undefined 
+ex: let name
+
+5. null
+ex let phone = null
+
+6. object
+
+7. symbol 
+*/
+
+// typeof is a keyword to know the datatype of variable
+
+console.log(typeof "hello")      // string
+let num = 13
+console.log(typeof num)          // number
+let value = null
+let data
+console.log(typeof value)        // object
+console.log(typeof data);        // undefined
