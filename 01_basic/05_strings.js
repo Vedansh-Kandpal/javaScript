e13cbb1fe7c5370e4add335858da6af454062b80
Vedansh Kandpal <95868825+Vedansh-Kandpal@users.noreply.github.com>
2024-02-29 01:03:56 +0000

    strings iñ js

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,35 @@
+let first = "vedansh"
+let second = "kandpal"
+
+console.log(`hello my name is ${first} ${second}`)
+
+const gameName = new String('Vedansh-almora')
+// console.log(gameName)
+
+//strings are not an are strings are object
+
+// console.log(gameName[0])
+// console.log(second[0]);
+
+//here above these are key value pair 
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase()); 
+// console.log(gameName.indexOf("V")); 
+
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)   // -ve value not support in substring
+
+// const newSlice = first.slice(-8, 2)
+
+// console.log(newSlice)
+
+// split string to an array bases of -
+
+console.log(gameName.split('-'))
+
+let newName = "  vinay             "
+console.log(newName)
+console.log(newName.trim())
+
