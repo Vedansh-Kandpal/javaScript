8f8f93256b9649f1f5bae83b37c2fa2664bcd533
Vedansh Kandpal <95868825+Vedansh-Kandpal@users.noreply.github.com>
2024-03-08 05:49:47 +0000

    callbacks in js

---
+++ 03_Advanced/Asynchronous/01_callbacks.js
@@ -0,0 +1,9 @@
+// Callbacks are functions that are passed as arguments to other functions and are executed when a certain event occurs. 
+// For example, the setTimeout function takes a function as its first argument and executes it after a specified number of milliseconds.
+
+// Use setTimeout to execute a callback after 1 second
+setTimeout(function() {
+    console.log("Hello, world!");
+  }, 1000);
+
+
