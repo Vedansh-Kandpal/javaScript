5231af7a745a996cc369db25c26f98817e8dcc70
Vedansh Kandpal <95868825+Vedansh-Kandpal@users.noreply.github.com>
2024-03-08 10:46:55 +0000

    async-await

---
+++ 03_Advanced/Asynchronous/03_asyncAwait.js
@@ -0,0 +1,19 @@
+/*
+
+
+The async keyword allows you to define a function that handles asynchronous operations.
+
+To define an async function, you place the async keyword in front of the function keyword as follows:
+
+async function sayHi() {
+    return 'Hi';
+}
+
+
+
+await keyword to wait for a Promise to settle either in a resolved or rejected state.
+
+
+
+*/
+
